8425159544a75ce111c47b001b6be49ee2651904
Joel Garcia <joel77270@gmail.com>
2025-06-07 22:03:23 -0300

    Se añadieron los boton a las cards [JoelGarcia]

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,21 @@ import {
     Image,
     Link,
     Center,
-    Text
+    Text,
+    Button,
+    Flex,
 } from "@chakra-ui/react";
 import { useAppColors } from "../../theme/colors";
 import { useColorModeValue } from "../components/ui/color-mode"
 import products from "../data/products.json";
 
-import {useProductos} from "../hooks/useProductos";
-import { color } from "framer-motion";
+import { useProductos } from "../hooks/useProductos";
+
 
 const Home = () => {
     const colors = useAppColors();
     const { productos } = useProductos();
     return (
-        
         <Box py={10} px={5}>
             <Text fontFamily={"sans-serif"} fontSize={30} mb={5} textAlign="center">Lista de productos</Text>
             <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} gap={10}>
@@ -34,9 +35,12 @@ const Home = () => {
                             transition: "0.3s",
                         }}
                     >
-                        <Box fontSize="xl" fontWeight="bold" color={colors.textPrimary} mb={2}>
-                            {producto.nombre}
-                        </Box>
+                        <Flex justify="space-between" align="center" mb={2}>
+                            <Box fontSize="xl" fontWeight="bold" color={colors.textPrimary}>
+                                {producto.nombre}
+                            </Box>
+                        </Flex>
+
                         <Center mb={4}>
                             <Link href={`producto-${producto.id}`} isExternal>
                                 <Image
@@ -52,9 +56,20 @@ const Home = () => {
                         <Box fontSize="md" color={colors.textSecondary} mb={3}>
                             {producto.descripcion}
                         </Box>
-                        <Box textAlign="center" fontSize="lg" fontWeight="semibold" color={colors.success}>
+                        <Box textAlign="center" fontSize="lg" mb={4} fontWeight="semibold" color={colors.success}>
                             ${producto.precio.toFixed(2)}
                         </Box>
+                        <Flex gap={3} wrap="wrap" justify="center">
+                            <Button bg={useColorModeValue("yellow.500", "yellow.500")} size="sm">
+                                Ver detalles
+                            </Button>
+                            <Button bg={useColorModeValue("blue.500", "blue.500")}  size="sm">
+                                Editar
+                            </Button>
+                            <Button bg={useColorModeValue("red.500", "red.500")}  size="sm">
+                                Eliminar
+                            </Button>
+                        </Flex>
                     </Box>
                 ))}
             </SimpleGrid>
